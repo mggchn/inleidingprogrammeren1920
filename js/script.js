5b082318b1f4747c15d849462992fdbd84a45334
Meggie Chen <meggiechn@gmail.com>
2020-06-22 07:28:22 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -2,47 +2,41 @@
 const insertQuestion = document.querySelector('h2');
 const qaContainer = document.getElementById('qa-container');
 
-// array van objects van de vragen en antwoorden.
+// Array van objects van de vragen en antwoorden.
 const qaList = [{
         question: "Question 1: What is the correct spelling of his name?",
         image: "../images/q1.jpg", 
-        options: ["Pharell Williams", "Pharrell Williams", "Pharrell William", "Pharrel William"],
+        options: ["Pharrell Williams", "Pharell Williams", "Pharrell William", "Pharrel William"],
         answer: "Pharrell Williams"
     },
     {
         question: "Question 2: Who was the most streamed artist on Spotify in 2019?",
         image: "../images/q2.jpg", 
-        options: ["Ariana Grande", "Ed Sheeran", "Post Malone", "Drake"],
+        options: ["Post Malone", "Ariana Grande", "Ed Sheeran", "Drake"],
         answer: "Post Malone"
     },
     {
         question: "Question 3: Who is the youngest member of the Beatles?",
         image: "../images/q3.jpg", 
-        options: ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"],
+        options: ["George Harrison", "Paul McCartney", "Ringo Starr", "John Lennon"],
         answer: "George Harrison"
     }, {
         question: "Question 4: What is David Bowie's real surname?",
         image: "../images/q4.jpg", 
-        options: ["David Jones", "David Robertson", "David Robert Bowie", "David Robert Jones"],
+        options: ["David Robert Jones", "David Jones", "David Robertson", "David Robert Bowie"],
         answer: "David Robert Jones"
     }
 ]
 
-
-// laden van de functie.
+// Laden van de functie.
 window.onload = function () {
     showQA();
 }
 
-// geen idee waar dit voor is, maar deze heb ik maar even bewaard. volgens mij iets met .push[i]
-let optionsArray = [];
-// console.log(optionsArray);
-
 function showQA() {
 
-    // als eerst worden er in deze for loop hieronder allerlei elementen aangemaakt die nodig zijn om de content te tonen. 
-    // deze loops worden aangemaakt om door als het ware door elke array van de lijst te 'loopen'.
-    // const questionList = qaList;
+    // Als eerst worden er in deze for loop hieronder allerlei elementen aangemaakt die nodig zijn om de content te tonen. 
+    // Deze loops worden aangemaakt om door als het ware door elke array van de lijst te 'loopen'.
     let i, j;
     for (i = 0; i < qaList.length; i++) {
 
@@ -62,69 +56,68 @@ function showQA() {
         // r.59-62 is voor de antwoorden.
         const aContainer = document.createElement('div');
         aContainer.classList.add('a-container');
-        const optionDiv = document.createElement('div');
-        optionDiv.classList.add('options');
 
         // DATA RETURNEN UIT DE QALIST
         showQuestion.textContent = qaList[i].question;
         showImage.src = qaList[i].image;
 
-        // dom-manipulatie: met .appendChild stop je de gewenste element binnen in de parent.
+        // Dom-manipulatie: met .appendChild stop je de gewenste element binnen in de parent.
         qaContainer.appendChild(qaBox);
         qaContainer.appendChild(qContainer);
-        qaContainer.appendChild(aContainer);
         
         qaBox.appendChild(qContainer);
         qaBox.appendChild(showImage);
         qaBox.appendChild(aContainer);
 
         qContainer.appendChild(showQuestion);
-        aContainer.appendChild(optionDiv);
 
-        // omdat er 4 opties gestored zijn in een array bij qaList, wordt hier weer een for loop gebruikt om de opties in de HTML te laten zien.
-        for (j = 0; j < qaList[i].options.length; j++) {
-            let options_box = qaList[i].options[j];
+        // In deze array worden de getallen bewaard om te zien welk getal al gemaakt is.
+        let ran_list = [];
+
+        // Omdat er 4 opties gestored zijn in een array bij qaList, wordt hier weer een for loop gebruikt om de opties in de HTML te laten zien.
+        for (j = 0; j < qaList[i].options.length; j) {
+            // console.log("j: " + j);
             const showOptions = document.createElement('div');
             showOptions.classList.add('option-box');
-            showOptions.innerHTML = options_box;
+            aContainer.appendChild(showOptions);
 
-            if (j == 0) {
-                showOptions.setAttribute('value', 'true');
-                showOptions.onclick = function () {
-                    showOptions.style.backgroundColor = 'green';
-                }
-            } else {
-                showOptions.setAttribute('value', 'false')
-                showOptions.onclick = function () {
-                    showOptions.style.backgroundColor = 'red';
+            // While loop blijft draaien zolang stop gelijk is aan false.
+            let stop = false;
+            while (stop == false) {
+
+                // Random getal tussen 0 t/m 4
+                let random = Math.floor(Math.random() * qaList[i].options.length);
+                // console.log("random: " + random);
+
+                // True als random getal gelijk is dan -1 (in andere woorden, random getal bestaat niet in ran_list[]).
+                // Alleen als het random getal nog niet bestaat, wordt de option element gevuld met de options_array_item.
+                if (ran_list.indexOf(random) == -1) {
+                    let options_array_item = qaList[i].options[random];
+                    showOptions.innerHTML = options_array_item;
+
+                    // Als index 0 is, dan dat je juiste antwoord en moet een het groene kleur krijgen. Zo niet, dan wordt het rood.
+                    if (random == 0) {
+                        showOptions.setAttribute('value', 'true');
+                        showOptions.onclick = function() {
+                            showOptions.style.backgroundColor = 'green';
+                        }
+                    } else {
+                        showOptions.setAttribute('value', 'false')
+                        showOptions.onclick = function() {
+                            showOptions.style.backgroundColor = 'red';
+                        }
+                    }
+
+                    ran_list.push(random);
+                    // console.log("ran_list: " + ran_list);
+
+                    j++;
+                    stop = true;
                 }
             }
-            
-            // deze for loop zorgt ervoor dat de 4 opties zelf hun eigen container hebben.
-           
-            optionDiv.appendChild(showOptions);
-
-            var optionButton = document.querySelector('.option-box');
-            // console.log(optionButton);
         }
     }
 }
-
-
-// de ingedrukte knop zal gekoppeld moeten worden aan de index vd array, hoe?
-// wanneer je op een optie drukt zal hij groen worden bij het goede antwoord.
-// zo niet, wordt de optie rood zijn en een andere button groen maken die het goede antwoord geeft.
-
-testButton = document.getElementById('test');
-
-testButton.onclick = function () {
-    console.log('het werkt');
-}
-
-
-
-
-
 
 // BRONVERMELDING
 // foto van pharrell williams: https://hypebeast.com/2020/4/pharrell-williams-iamother-soundcloud-compilation-album-emerging-talent-search
